e00d964682a33ab502994b0ca03ab444d30cdd41
Roland Kappeler <roland.kappeler@zuehlke.com>
2017-01-31 14:02:46 +0100

    whatever

---
+++ UnoWithFSharp/UnoWithFSharpTest/Program.fs
@@ -0,0 +1,7 @@
+﻿open Expecto
+open Tests
+
+[<EntryPoint>]
+let main argv = 
+    runTests defaultConfig tests |> ignore
+    0 // return an integer exit code
---
+++ UnoWithFSharp/UnoWithFSharpTest/Tests.fs
@@ -0,0 +1,32 @@
+﻿module Tests
+
+open Game
+open Expecto
+
+
+
+[<Tests>]
+let tests =
+    testList "samples"  [
+        testCase "1 should be 1" <| fun _ ->
+            Expect.equal 1 1 ""
+        testCase "universe exists" <| fun _ ->
+            let result =
+                Game.decide 
+                    (StartGame { Players = 3; FirstCard = Digit(Three, Red)}) 
+                    InitialState
+            Expect.equal 
+                (Ok [ GameStarted { Players = 3; FirstCard = Digit(Three, Red)} ] )
+                result
+                "Game should be started"        
+                
+        testCase "universe exists" <| fun _ ->
+            let result =
+                Game.decide 
+                    (StartGame { Players = 3; FirstCard = Digit(Three, Red)}) 
+                    InitialState
+            Expect.equal 
+                (Ok [ GameStarted { Players = 3; FirstCard = Digit(Three, Red)} ] )
+                result
+                "Game should be started"
+    ]
