c9f5b37d3885b5172be5e6103479e586ef159478
Roland Kappeler <roland.kappeler@zuehlke.com>
2017-01-31 11:53:48 +0100

    game fs

---
+++ UnoWithFSharp/UnoWithFSharp/Game.fs
@@ -0,0 +1,14 @@
+﻿module Game
+
+type Command = | DoSomething
+
+type Event = | SomeThingHappened
+
+type State = | InitialState
+
+let decide command state = []
+//    match command with
+//    | DoSomething -> seq [ SomeThingHappened ]
+
+let evolve (state:State) (event:Event) : State =
+    state
