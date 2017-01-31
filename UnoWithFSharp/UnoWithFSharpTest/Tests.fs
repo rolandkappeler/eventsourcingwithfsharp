45d38438880bad7d5f058887b2350d043d0fd9e3
Vincent Schippefilt <vincent.schippefilt@gfi.be>
2017-01-31 14:13:26 +0100

    updated tests

--- UnoWithFSharp/UnoWithFSharpTest/Tests.fs
+++ UnoWithFSharp/UnoWithFSharpTest/Tests.fs
@@ -21,25 +21,15 @@ let tests =
     testList "samples"  [
         testCase "1 should be 1" <| fun _ ->
             Expect.equal 1 1 ""
+
         testCase "Game should start" <| fun _ ->
-            let result =
-                Game.decide 
-                    (StartGame { Players = 3; FirstCard = Digit(Three, Red)}) 
-                    InitialState
-            Expect.equal 
-                (Ok [ GameStarted { Players = 3; FirstCard = Digit(Three, Red)} ] )
-                result
-                "Game should be started"        
+            [] 
+            => StartGame { Players = 3; FirstCard = Digit(Three, Red)}
+            == [ GameStarted { Players = 3; FirstCard = Digit(Three, Red)} ] 
+     
                 
         testCase "Cannot start game twice" <| fun _ ->
-            let firstGame = GameStarted { Players = 2; FirstCard = Digit(Three, Red)}
-            let state = evolve InitialState firstGame 
-            let result =
-                Game.decide 
-                    (StartGame { Players = 3; FirstCard = Digit(Three, Red)}) 
-                    state
-            Expect.equal 
-                (Failure "Game already started" )
-                result
-                "Game should be started"
+            [GameStarted { Players = 2; FirstCard = Digit(Three, Red)}]
+            => StartGame { Players = 3; FirstCard = Digit(Three, Red)}
+            =! GameAlreadyStarted
     ]
