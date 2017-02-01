348064ac32149032b59fffbafb1c2a9ddd52a8b2
Vincent Schippefilt <vincent.schippefilt@gfi.be>
2017-02-01 11:43:37 +0100

    ok

--- UnoWithFSharp/UnoWithFSharp/Program.fs
+++ UnoWithFSharp/UnoWithFSharp/Program.fs
@@ -1,7 +1,16 @@
 ﻿// Learn more about F# at http://fsharp.org
 // See the 'F# Tutorial' project for more help.
+open CommandHandler
+open EventStore
+open EventStore.ClientAPI
 
 [<EntryPoint>]
 let main argv = 
+    let connection = System.Uri("tcp://admin:changeit@localhost:2112")
+    let eventStoreConnection = EventStore.ClientAPI.EventStoreConnection.Create(connection)
+    let theRead = EventStore.read eventStoreConnection 
+    handler (read eventStoreConnection "whatever" 0) () "whatever" StartGame
+
+
     printfn "%A" argv
     0 // return an integer exit code
